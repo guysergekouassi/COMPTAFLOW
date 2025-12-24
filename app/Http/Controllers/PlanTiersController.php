f8f900cb93822862557653fc92aa6d00ab12b762
Guysergekouassi <williamskouassi525@gmail.com>
2025-12-24 09:00:18 +0100

    modif sur plan tiers 1

--- app/Http/Controllers/PlanTiersController.php
+++ app/Http/Controllers/PlanTiersController.php
@@ -6,6 +6,7 @@ use App\Models\PlanTiers;
 use App\Models\PlanComptable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 class PlanTiersController extends Controller
 {
