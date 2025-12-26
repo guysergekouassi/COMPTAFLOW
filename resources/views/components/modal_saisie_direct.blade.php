a241e185d27a5e0f123e84dff4782b76af7c477d
Guysergekouassi <williamskouassi525@gmail.com>
2025-12-26 22:58:32 +0100

    Correction DOUBLON

--- resources/views/components/modal_saisie_direct.blade.php
+++ resources/views/components/modal_saisie_direct.blade.php
@@ -81,6 +81,8 @@
     </div>
 </div>
 
+<!-- Ajout de jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     // Correction robuste contre les doublons d'affichage
     $(document).ready(function() {
