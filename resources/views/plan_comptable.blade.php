f30b237a6c265a5ee050390e58ab53e56518cadf
Guysergekouassi <williamskouassi525@gmail.com>
2025-12-30 10:08:44 +0100

    modifs plan 3

--- resources/views/plan_comptable.blade.php
+++ resources/views/plan_comptable.blade.php
@@ -566,7 +566,7 @@
                 table = $('#planComptableTable').DataTable({
                     processing: true,
                     serverSide: true,
-                    ajax: "{{ route('plan_comptable.datatable') }}",
+                    ajax: "{{  }}",
                     columns: [
                         { data: 'numero_de_compte', name: 'numero_de_compte' },
                         { data: 'intitule', name: 'intitule' },
