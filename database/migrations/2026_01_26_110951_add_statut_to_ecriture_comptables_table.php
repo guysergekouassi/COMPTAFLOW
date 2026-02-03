e144a588982c3aee0e9fbf15e29964bcc9571a5f
Guysergekouassi <williamskouassi525@gmail.com>
2026-02-03 12:20:29 +0100

    mise ajour

---
+++ database/migrations/2026_01_26_110951_add_statut_to_ecriture_comptables_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('ecriture_comptables', function (Blueprint $table) {
+            $table->string('statut')->default('draft'); // ou 'approved' si tu veux
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('ecriture_comptables', function (Blueprint $table) {
+            $table->dropColumn('statut');
+        });
+    }
+};
